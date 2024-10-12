afe1f8489284048dd46b2ddb11f004704c4b8ebd
Cupz <78942813+GabrielaPeroni@users.noreply.github.com>
2024-10-12 11:57:06 -0300

    Update galeria.py

--- galeria.py
+++ galeria.py
@@ -1,13 +1,13 @@
 from customtkinter import CTkFrame, CTkLabel, CTkButton, CTkImage, CTkCanvas
 from PIL import Image
 from favoritos import toggle_favorite, on_enter, on_leave
-from categorias import all_items
 
 def clear_frame(frame):
     for widget in frame.winfo_children():
         widget.destroy()
 
-def show_content(main_frame, items):
+# Menu da categoria
+def show_content(main_frame, items,  app):
     from favoritos import favorite_states
     clear_frame(main_frame)
 
@@ -48,17 +48,17 @@ def show_content(main_frame, items):
         initial_icon = "icons/favorito_active.png" if favorite_states.get(item["name"], False) else "icons/favorito.png"
         favorite_button.configure(image=CTkImage(Image.open(initial_icon), size=(20, 20)))
 
-        button_details = CTkButton(master=frame, text="Ver mais", command=lambda p=item: show_gallery(main_frame, p, items), fg_color="red")
+        button_details = CTkButton(master=frame, text="Ver mais", command=lambda p=item: show_gallery(main_frame, p, items,  app), fg_color="red")
         button_details.pack(pady=(0, 10), padx=(0, 10), anchor="e")
 
-
-def show_gallery(main_frame, place, all_items):
+# menu de 'Ver Mais'
+def show_gallery(main_frame, place, all_items, app):
     clear_frame(main_frame)
 
     global last_clicked_category
     last_clicked_category = all_items
 
-    back_button = CTkButton(master=main_frame, text="Voltar", command=lambda: show_content(main_frame, last_clicked_category), fg_color="red")
+    back_button = CTkButton(master=main_frame, text="Voltar", command=lambda: show_content(main_frame, last_clicked_category,  app), fg_color="red")
     back_button.pack(pady=(10, 5), padx=(10, 0), anchor="nw")
 
     try:
@@ -70,28 +70,28 @@ def show_gallery(main_frame, place, all_items):
     except Exception as e:
         print(f"Erro ao carregar a imagem: {place['image']} - {e}")
 
-    label_name = CTkLabel(master=main_frame, text=place["name"], font=("Arial", 18, "bold"))
-    label_name.pack(pady=(10, 5))
+    label_name = CTkLabel(master=main_frame, text=place["name"], font=("Arial", 23, "bold"))
+    label_name.pack(pady=(5, 5))
 
     label_description = CTkLabel(master=main_frame, text=place["description"], wraplength=400)
     label_description.pack(pady=(5, 10))
 
     gallery_images = place.get("gallery", [])
     if gallery_images:
-        gallery_canvas = CTkCanvas(main_frame, height=150)
-        gallery_canvas.pack(pady=10, fill="both", expand=True)
+        gallery_canvas = CTkCanvas(main_frame, height=150, highlightthickness=0 ,bg=app.cget("bg"), relief="sunken")
+        gallery_canvas.pack(pady=5, padx = 10, fill="both", expand=True)
 
-        gallery_container = CTkFrame(gallery_canvas)
-        gallery_canvas.create_window((0, 0), window=gallery_container, anchor="nw")
+        gallery_container = CTkFrame(gallery_canvas, fg_color=app.cget("bg"))
+        gallery_canvas.create_window(gallery_canvas.winfo_width() // 200, 75, window=gallery_container, anchor="center")
 
         for image_path in gallery_images:
             try:
                 img = Image.open(image_path)
                 image = CTkImage(img, size=(120, 90))
                 gallery_label = CTkButton(master=gallery_container, image=image, text="", 
-                                          command=lambda img=image_path: update_main_image(main_image_label, img))
+                                          command=lambda img=image_path: update_main_image(main_image_label, img), fg_color=app.cget("bg"))
                 gallery_label.image = image
-                gallery_label.pack(side="left", padx=5)
+                gallery_label.pack(side="left", padx=10, pady = 5)
             except Exception as e:
                 print(f"Erro ao carregar a imagem da galeria: {image_path} - {e}")
 
